50c34d335b2966d35512bf6a629a58398198f157
Jake Shin <ninano112@gmail.com>
2026-05-28 09:58:23 +0900

    Point gateway at current qrcoding deployment

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_QRCODING_BASE_URL =
-  "https://qrcoding-7l4aqfjr0-jakes-projects-0ab50f91.vercel.app";
+  "https://qrcoding-contentscoin-jakes-projects-0ab50f91.vercel.app";
 
 export function getQrcodingBaseUrl(): string {
   return (process.env.QRCODING_BASE_URL ?? DEFAULT_QRCODING_BASE_URL).replace(/\/$/, "");
